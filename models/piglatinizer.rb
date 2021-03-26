43e67f68dae7d040ead379dea99879c5ac6727f4
Ben Hayes <b.t.hayes@comcast.net>
2021-03-26 17:50:03 -0500

    commit tree 2f56c2486fc86429371dd109da6a29010b2e6b4b

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
   def piglatinize(text)
     @piglatin = ""
     arr1 = text.split
-    if arr1.size > 1
+    #if arr1.size > 1
       arr3 = arr1.map do |w|
         if w.downcase.start_with?(/[aeiou]/)
           "#{w}way"
@@ -19,14 +19,14 @@ class PigLatinizer
         end
       end
       @piglatin = arr3.join(" ")
-    else
-      if text.downcase.start_with?(/[aeiou]/)
-        @piglatin = "#{text}way"
-      else
-        arr3 = text.split(/([aeiou].*)/)
-        @piglatin = "#{arr3[1]}#{arr3[0]}ay"
-      end
-    end
+    #else
+      #if text.downcase.start_with?(/[aeiou]/)
+      #  @piglatin = "#{text}way"
+      #else
+      #  arr3 = text.split(/([aeiou].*)/)
+      #  @piglatin = "#{arr3[1]}#{arr3[0]}ay"
+      #end
+    #end
     @piglatin
   end
 
